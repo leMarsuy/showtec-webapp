8ae87ce9476ac9ea79ec5073b2b1100d66cd08f6
Marci <marcilianochavezIII@gmail.com>
2024-07-29 10:52:14 +0800

    fixed build prob

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ import { commonEnv } from './environment.common';
 const env = {
   ENVIRONMENT_NAME: 'development',
   PRODUCTION: false,
-  API_URL: 'http://localhost:3500/api/v1',
+  API_URL: 'https://sea-turtle-app-jwxg7.ondigitalocean.app/api/v1',
 };
 
 export const enviroment = {
