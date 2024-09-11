2bd66cf7ea2ed78cb283f30335abc27834d4ee63
Christian Alcaraz <christianmalcaraz@gmail.com>
2024-09-11 19:35:27 +0800

    wip account settings

--- src/app/modules/portal/components/header/header.component.ts
+++ src/app/modules/portal/components/header/header.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '@shared/services/api';
 import { User } from '@core/models/user.model';
+import { HttpErrorResponse } from '@angular/common/http';
+import { SnackbarService } from '@app/shared/components/snackbar/snackbar.service';
+import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { AccountSettingsComponent } from './account-settings/account-settings.component';
 
 @Component({
   selector: 'app-header',
@@ -12,11 +17,50 @@ export class HeaderComponent implements OnInit {
   logoSrc = 'images/logo.png';
   me!: User;
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private snackbarService: SnackbarService,
+    private router: Router,
+    private readonly dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
     this.authService.me().subscribe((res) => {
       this.me = res;
+      console.log(this.me);
+    });
+  }
+
+  onAccountSettings() {
+    const dialogRef = this.dialog.open(AccountSettingsComponent, {
+      data: {
+        user: this.me,
+      },
+      height: '56%',
+      width: 'calc(100% - 144px)',
+      maxWidth: '100%',
+
+      disableClose: true,
+      autoFocus: false,
+    });
+
+    dialogRef.afterClosed().subscribe(() => {
+      //do something
+    });
+  }
+
+  onLogout() {
+    this.authService.logout().subscribe({
+      error: ({ error }: HttpErrorResponse) => {
+        this.snackbarService.openErrorSnackbar(error.errorCode, error.message);
+      },
+      complete: () => {
+        this.snackbarService.openSuccessSnackbar(
+          'Logged Out',
+          'You have logged out'
+        );
+        this.router.navigate(['/auth/login']);
+      },
     });
   }
 }
