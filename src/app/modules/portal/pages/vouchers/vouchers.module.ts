8ad4d6a18a0cae0b220fd081dae52a0c04f03958
Christian Alcaraz <christianmalcaraz@gmail.com>
2024-09-14 04:04:49 +0800

    updated voucher component ui

--- src/app/modules/portal/pages/vouchers/vouchers.module.ts
+++ src/app/modules/portal/pages/vouchers/vouchers.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { VouchersRoutingModule } from './vouchers-routing.module';
-
+import { ContentHeaderModule } from '@app/shared/components/content-header/content-header.module';
+import { RouterModule } from '@angular/router';
+import { VouchersComponent } from './vouchers.component';
+import { VouchersListModule } from './pages/vouchers-list/vouchers-list.module';
+import { UpsertVoucherModule } from './pages/upsert-voucher/upsert-voucher.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [VouchersComponent],
   imports: [
     CommonModule,
-    VouchersRoutingModule
-  ]
+    VouchersRoutingModule,
+    ContentHeaderModule,
+    RouterModule,
+    VouchersListModule,
+    UpsertVoucherModule,
+  ],
 })
-export class VouchersModule { }
+export class VouchersModule {}
