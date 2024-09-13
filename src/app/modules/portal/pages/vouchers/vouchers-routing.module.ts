462c211f78bbb916c148d1bf382a621ba1cfe41b
Christian Alcaraz <christianmalcaraz@gmail.com>
2024-09-14 04:00:53 +0800

    updated voucher routing module

--- src/app/modules/portal/pages/vouchers/vouchers-routing.module.ts
+++ src/app/modules/portal/pages/vouchers/vouchers-routing.module.ts
@@ -2,7 +2,35 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { VouchersComponent } from './vouchers.component';
 
-const routes: Routes = [{ path: '', component: VouchersComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: VouchersComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('./pages/vouchers-list/vouchers-list.module').then(
+            (m) => m.VouchersListModule
+          ),
+      },
+      {
+        path: 'create',
+        loadChildren: () =>
+          import('./pages/upsert-voucher/upsert-voucher.module').then(
+            (m) => m.UpsertVoucherModule
+          ),
+      },
+      {
+        path: 'edit/:_id',
+        loadChildren: () =>
+          import('./pages/upsert-voucher/upsert-voucher.module').then(
+            (m) => m.UpsertVoucherModule
+          ),
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
