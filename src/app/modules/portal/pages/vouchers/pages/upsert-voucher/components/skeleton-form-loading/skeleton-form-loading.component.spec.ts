4f15b32a7d16cfb10e1b7e5da5b742d116ba3d8c
Christian Alcaraz <christianmalcaraz@gmail.com>
2024-09-18 02:14:00 +0800

    added skeletoon form loading component

---
+++ src/app/modules/portal/pages/vouchers/pages/upsert-voucher/components/skeleton-form-loading/skeleton-form-loading.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SkeletonFormLoadingComponent } from './skeleton-form-loading.component';
+
+describe('SkeletonFormLoadingComponent', () => {
+  let component: SkeletonFormLoadingComponent;
+  let fixture: ComponentFixture<SkeletonFormLoadingComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [SkeletonFormLoadingComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(SkeletonFormLoadingComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
