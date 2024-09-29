3a94386b9f6b10dbba5b3b5e039ca5b890f1a284
Christian Alcaraz <christianmalcaraz@gmail.com>
2024-09-30 04:28:00 +0800

    add purchase order resolver for route edit purchase order

---
+++ src/app/modules/portal/pages/purchase-order/purchase-order.resolver.ts
@@ -0,0 +1,31 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { inject } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { PurchaseOrderApiService } from '@app/shared/services/api/purchase-order-api/purchase-order-api.service';
+import { catchError, of } from 'rxjs';
+
+export const purchaseOrderResolver = (
+  route: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot
+) => {
+  const router = inject(Router);
+  const purchaseOrderApi = inject(PurchaseOrderApiService);
+  const purchaseOrderId = route.paramMap.get('id');
+
+  if (!purchaseOrderId) {
+    router.navigate(['portal/purchase-order/list']);
+    return null;
+  }
+
+  return purchaseOrderApi.getPurchaseOrderById(purchaseOrderId).pipe(
+    catchError((error) => {
+      console.error(error);
+      router.navigate(['portal/purchase-order/list']);
+      return of('PO not found');
+    })
+  );
+};
