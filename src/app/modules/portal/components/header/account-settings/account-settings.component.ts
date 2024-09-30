802956a022503a93ef2d2801c619a55c291e174b
Christian Alcaraz <christianmalcaraz@gmail.com>
2024-09-30 21:39:01 +0800

    adds onPasswordRequest method for account settings component

--- src/app/modules/portal/components/header/account-settings/account-settings.component.ts
+++ src/app/modules/portal/components/header/account-settings/account-settings.component.ts
@@ -2,9 +2,11 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 import { User } from '@app/core/models/user.model';
 import { ConfirmationService } from '@app/shared/components/confirmation/confirmation.service';
 import { SnackbarService } from '@app/shared/components/snackbar/snackbar.service';
+import { AuthService } from '@app/shared/services/api';
 import { UserApiService } from '@app/shared/services/api/user-api/user-api.service';
 import { filter, map, Subject, switchMap, takeUntil } from 'rxjs';
 
@@ -30,7 +32,9 @@ export class AccountSettingsComponent implements OnInit, OnDestroy {
     private _dialogRef: MatDialogRef<AccountSettingsComponent>,
     private confirmation: ConfirmationService,
     private snackbar: SnackbarService,
-    private userApi: UserApiService
+    private userApi: UserApiService,
+    private authApi: AuthService,
+    private router: Router
   ) {
     if (!data.user) throw new Error('User not found');
     this.user = data.user;
@@ -79,8 +83,45 @@ export class AccountSettingsComponent implements OnInit, OnDestroy {
       });
   }
 
-  onClose() {
-    this._dialogRef.close();
+  onPasswordResetRequest() {
+    this.confirmation
+      .open(
+        'Password Reset',
+        `<p class='text-base'>Would you like to change your password?</p> <br/><p class='text-gray-500 text-sm'>The reset link will be sent to your email and you'll be logout. Please save your activities before proceeding.</p> <br/>`
+      )
+      .afterClosed()
+      .pipe(
+        filter((result) => result),
+        switchMap(() => {
+          this.loading = true;
+          this.snackbar.openLoadingSnackbar('Please Wait', 'Sending Email...');
+          return this.authApi.forgotPassword(this.user.email);
+        })
+      )
+      .subscribe({
+        next: () => {
+          this.loading = false;
+          this.snackbar.closeLoadingSnackbar();
+          this._dialogRef.close();
+          this.authApi.logout().subscribe({
+            error: ({ error }: HttpErrorResponse) => {
+              this.snackbar.openErrorSnackbar(error.errorCode, error.message);
+            },
+            complete: () => {
+              this.snackbar.openSuccessSnackbar(
+                'Logged Out',
+                'You have logged out'
+              );
+              this.router.navigate(['/auth/login']);
+            },
+          });
+        },
+        error: ({ error }: HttpErrorResponse) => {
+          this.loading = false;
+          console.log(error);
+          this.snackbar.openErrorSnackbar(error?.errorCode, error?.message);
+        },
+      });
   }
 
   onInputChange(formControl: FormControl) {
