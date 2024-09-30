721dc9ff6ab5c98aaa124f2042f4172bcf1b7762
Christian Alcaraz <christianmalcaraz@gmail.com>
2024-09-30 21:10:33 +0800

    add reset password component

--- src/app/modules/auth/pages/reset-password/reset-password.component.ts
+++ src/app/modules/auth/pages/reset-password/reset-password.component.ts
@@ -1,12 +1,126 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RegexValidator } from '@app/core/enums/regex-validators.enum';
+import { SnackbarService } from '@app/shared/components/snackbar/snackbar.service';
+import { AuthService } from '@app/shared/services/api';
+import { regexValidator } from '@app/shared/validators/regex.validator';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-reset-password',
-  standalone: true,
-  imports: [],
   templateUrl: './reset-password.component.html',
-  styleUrl: './reset-password.component.scss'
+  styleUrl: './reset-password.component.scss',
 })
 export class ResetPasswordComponent {
+  resetFieldsConfig = {
+    password: {
+      label: 'New Password',
+      name: 'password',
+      removeHint: true,
+    },
+    confirmPassword: {
+      type: 'password',
+      label: 'Confirm Password',
+      name: 'confirmPassword',
+    },
+  };
 
+  validations = [
+    {
+      type: 'one-number',
+      regex: RegexValidator.ATLEAST_ONE_NUMBER,
+    },
+    {
+      type: 'one-special-character',
+      regex: RegexValidator.ATLEAST_ONE_SPECIAL_CHARACTER,
+    },
+    {
+      type: 'one-upper-lower-case',
+      regex: RegexValidator.ATLEAST_ONE_LOWER_UPPER_CASE_CHARACTER,
+    },
+  ];
+
+  resetPasswordForm: FormGroup;
+  token: string;
+
+  private _isLoading$: BehaviorSubject<boolean> = new BehaviorSubject(false);
+
+  get isLoading$() {
+    return this._isLoading$.asObservable();
+  }
+
+  get passwordFcErrors() {
+    return this.resetPasswordForm.get('password')?.errors || {};
+  }
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private snackbarService: SnackbarService,
+    private authService: AuthService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {
+    this.token = this.activatedRoute.snapshot.paramMap.get('token') || '';
+    const passwordValidators = [Validators.required, Validators.minLength(8)];
+
+    this.validations.forEach((validation) => {
+      passwordValidators.push(regexValidator(validation));
+    });
+
+    this.resetPasswordForm = this.formBuilder.group({
+      password: ['', passwordValidators],
+      confirmPassword: ['', Validators.required],
+    });
+  }
+
+  confirmPasswordClick() {
+    if (
+      this.resetPasswordForm.invalid ||
+      this.resetPasswordForm.value.password !==
+        this.resetPasswordForm.value.confirmPassword
+    ) {
+      return;
+    }
+
+    this._setLoadingstate(true);
+
+    this.authService
+      .resetPassword({
+        password: this.resetPasswordForm.value.password,
+        token: this.token,
+      })
+      .subscribe({
+        next: () => {
+          this._setLoadingstate(false);
+          this.snackbarService.openSuccessSnackbar(
+            'You have changed your password successfully. Please login.'
+          );
+          this.router.navigate(['auth', 'login']);
+        },
+        error: (err) => {
+          this._setLoadingstate(false);
+          this.snackbarService.openErrorSnackbar(err.error.message);
+        },
+      });
+  }
+
+  onBackToLogin() {
+    this.router.navigate(['auth/login']);
+  }
+
+  private _setLoadingstate(isLoading: boolean) {
+    this._isLoading$.next(isLoading);
+
+    if (isLoading) {
+      this.snackbarService.openLoadingSnackbar(
+        'Please wait...',
+        `Changing your password.`
+      );
+      this.resetPasswordForm.disable();
+    } else {
+      this.snackbarService.closeLoadingSnackbar();
+      this.resetPasswordForm.enable();
+    }
+  }
 }
--- src/app/modules/auth/pages/reset-password/reset-password.module.ts
+++ src/app/modules/auth/pages/reset-password/reset-password.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ResetPasswordRoutingModule } from './reset-password-routing.module';
-
+import { ResetPasswordComponent } from './reset-password.component';
+import { PasswordInputModule } from '../../components/password-input/password-input.module';
+import { MatIconModule } from '@angular/material/icon';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [],
+  declarations: [ResetPasswordComponent],
   imports: [
     CommonModule,
-    ResetPasswordRoutingModule
-  ]
+    ResetPasswordRoutingModule,
+    PasswordInputModule,
+    MatIconModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+  ],
+  exports: [ResetPasswordComponent],
 })
-export class ResetPasswordModule { }
+export class ResetPasswordModule {}
