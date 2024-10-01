aa011ade316ea6e749c3d1a09ec21b82408fe294
Christian Alcaraz <christianmalcaraz@gmail.com>
2024-10-01 11:56:28 +0800

    implementation of auth guard in auth routing

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth.component';
 import { resetPasswordGuard } from './pages/reset-password/reset-password.guard';
+import { authGuard } from './auth.guard';
 
 const routes: Routes = [
   {
     path: '',
     component: AuthComponent,
+    canActivate: [authGuard],
     children: [
       {
         path: '',
