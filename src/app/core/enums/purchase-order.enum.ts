07aeaa90c27613ed91a91887b507459132e1a766
Christian Alcaraz <christianmalcaraz@gmail.com>
2024-10-02 16:10:33 +0800

    adds active status in purchase order status enum

--- src/app/core/enums/purchase-order.enum.ts
+++ src/app/core/enums/purchase-order.enum.ts
@@ -3,6 +3,7 @@ export enum PurchaseOrderStatus {
   PAID = 'Paid',
   CANCELLED = 'Cancelled',
   PARTIAL = 'Partial',
+  ACTIVE = 'Active',
 }
 
 export const PURCHASE_ORDER_STATUSES = Object.values(PurchaseOrderStatus);
