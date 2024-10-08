0234e24cd971e71ab86c6434164e192520da1d04
Christian Alcaraz <christianmalcaraz@gmail.com>
2024-10-08 18:55:55 +0800

    creates total-summary-display shared module

---
+++ src/app/shared/components/total-summary-display/total-summary-display.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TotalSummaryDisplayComponent } from './total-summary-display.component';
+
+@NgModule({
+  declarations: [TotalSummaryDisplayComponent],
+  imports: [CommonModule],
+  exports: [TotalSummaryDisplayComponent],
+})
+export class TotalSummaryDisplayModule {}
