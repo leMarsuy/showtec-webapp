0432058d3d1282b735e80faa556d35fd4d3bb7fb
Christian Alcaraz <christianmalcaraz@gmail.com>
2024-10-12 01:55:06 +0800

    creates attach customer form component

---
+++ src/app/shared/forms/attach-customer-form/attach-customer-form.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { TableModule } from '@app/shared/components/table/table.module';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { AttachCustomerFormComponent } from './attach-customer-form.component';
+
+@NgModule({
+  declarations: [AttachCustomerFormComponent],
+  imports: [
+    CommonModule,
+    TableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+    MatDatepickerModule,
+  ],
+  exports: [AttachCustomerFormComponent],
+})
+export class AttachCustomerFormModule {}
