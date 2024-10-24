c3b459b55c931267db84021aa1ac2586c6cd1d7b
Christian Alcaraz <christianmalcaraz@gmail.com>
2024-10-24 21:37:29 +0800

    adds skeleton-loading for attach customer form shared component

---
+++ src/app/shared/forms/attach-customer-form/skeleton-loading/skeleton-loading.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'attach-customer-skeleton-loading',
+  templateUrl: './skeleton-loading.component.html',
+  styleUrl: './skeleton-loading.component.scss',
+})
+export class SkeletonLoadingComponent {}
