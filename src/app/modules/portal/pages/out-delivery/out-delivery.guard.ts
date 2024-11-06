761c3d676f9ec18bdeef1626081c95683c8a859d
Christian Alcaraz <christianmalcaraz@gmail.com>
2024-11-07 05:46:29 +0800

    add out delivery guard

---
+++ src/app/modules/portal/pages/out-delivery/out-delivery.guard.ts
@@ -0,0 +1,43 @@
+import { inject } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivateFn,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { OutDeliveryStatus } from '@app/core/enums/out-delivery-status.enum';
+import { OutDelivery } from '@app/core/models/out-delivery.model';
+import { OutDeliveryApiService } from '@app/shared/services/api/out-delivery-api/out-delivery-api.service';
+import { catchError, map, of } from 'rxjs';
+
+export const editOutDeliveryGuard: CanActivateFn = (
+  route: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot
+) => {
+  const outDeliveryApi = inject(OutDeliveryApiService);
+  const outDeliveryId = route.paramMap.get('_id');
+  console.log(outDeliveryId);
+  const router = inject(Router);
+
+  const allowedStatus: any = [
+    OutDeliveryStatus.ACTIVE,
+    OutDeliveryStatus.PENDING,
+  ];
+
+  if (!outDeliveryId) {
+    router.navigate(['portal/purchase-order/list']);
+    return false;
+  }
+
+  return outDeliveryApi.getOutDeliveryById(outDeliveryId).pipe(
+    map((response) => {
+      const dr = response as OutDelivery;
+      if (dr && allowedStatus.includes(dr.status)) {
+        return true;
+      } else {
+        return false;
+      }
+    }),
+    catchError(() => of(false))
+  );
+};
