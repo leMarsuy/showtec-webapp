e83f950849dbef5fa92356578ab8b4dbcc5b6140
Christian Alcaraz <christianmalcaraz@gmail.com>
2024-11-09 04:37:34 +0800

    ui enhancements

--- src/app/modules/portal/pages/settings/pages/voucher-settings/voucher-settings.component.ts
+++ src/app/modules/portal/pages/settings/pages/voucher-settings/voucher-settings.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { FormArray, FormBuilder } from '@angular/forms';
-import { catchError, Observable, of, Subject } from 'rxjs';
+import { catchError, filter, Observable, of, Subject, switchMap } from 'rxjs';
 import { MatDialog } from '@angular/material/dialog';
 import { ObjectService } from '@app/shared/services/util/object/object.service';
 import { SnackbarService } from '@app/shared/components/snackbar/snackbar.service';
@@ -11,6 +11,7 @@ import {
 } from '../../../vouchers/pages/upsert-voucher/account-title.list';
 import { ConfigApiService } from '@app/shared/services/api/config-apo/config-api.service';
 import { DistributionOfAccountDataService } from './voucher-settings-data.service';
+import { ConfirmationService } from '@app/shared/components/confirmation/confirmation.service';
 
 interface DistributionOfAccountsConfig {
   accountCategories: Array<string>;
@@ -34,12 +35,10 @@ export class VoucherSettingsComponent implements OnInit {
   config!: VoucherConfig;
 
   constructor(
-    private readonly fb: FormBuilder,
-    private readonly dialog: MatDialog,
-    private readonly objectService: ObjectService,
     private readonly configApi: ConfigApiService,
+    private readonly distributionOfAccountDataService: DistributionOfAccountDataService,
     private readonly snackbar: SnackbarService,
-    private readonly distributionOfAccountDataService: DistributionOfAccountDataService
+    private readonly confirmation: ConfirmationService
   ) {}
 
   ngOnInit(): void {
@@ -59,7 +58,7 @@ export class VoucherSettingsComponent implements OnInit {
     });
   }
 
-  onCategoriesSettingSave() {
+  onDistributionOfAccountSettingSave() {
     const newCategories = this.distributionOfAccountDataService.getCategories();
     const newTitles = this.distributionOfAccountDataService.getTitles();
     const body: Partial<VoucherConfig> = {
@@ -71,29 +70,38 @@ export class VoucherSettingsComponent implements OnInit {
       },
     };
 
-    this.snackbar.openLoadingSnackbar(
-      'Saving Account Categories',
-      'Please Wait...'
-    );
-    this.configApi.updateConfigByName(this.configName, body).subscribe({
-      next: (config) => {
-        this.snackbar.closeLoadingSnackbar();
-        this.snackbar.openSuccessSnackbar(
-          'Saving Success!',
-          'Distribution of Account Categories is updated.'
-        );
-        this.config = config as VoucherConfig;
-        this._setDistributionOfAccountSettings();
-      },
-      error: ({ error }: HttpErrorResponse) => {
-        this.snackbar.closeLoadingSnackbar();
-        console.error(error);
-        this.snackbar.openErrorSnackbar(
-          error.errorCode,
-          error.message + `Please reload the page`
-        );
-      },
-    });
+    this.confirmation
+      .open('Save Confirmation', 'Do you want to save your changes?')
+      .afterClosed()
+      .pipe(
+        filter((result) => result),
+        switchMap(() => {
+          this.snackbar.openLoadingSnackbar(
+            'Saving Account Categories',
+            'Please Wait...'
+          );
+          return this.configApi.updateConfigByName(this.configName, body);
+        })
+      )
+      .subscribe({
+        next: (config) => {
+          this.snackbar.closeLoadingSnackbar();
+          this.snackbar.openSuccessSnackbar(
+            'Saving Success!',
+            'Distribution of Account Categories is updated.'
+          );
+          this.config = config as VoucherConfig;
+          this._setDistributionOfAccountSettings();
+        },
+        error: ({ error }: HttpErrorResponse) => {
+          this.snackbar.closeLoadingSnackbar();
+          console.error(error);
+          this.snackbar.openErrorSnackbar(
+            error.errorCode,
+            error.message + `Please reload the page`
+          );
+        },
+      });
   }
 
   private _setDistributionOfAccountSettings() {
