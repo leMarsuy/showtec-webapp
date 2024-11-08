19cd91cd4cc42d4d421a0bdcfb1b47a084684f3d
Christian Alcaraz <christianmalcaraz@gmail.com>
2024-11-09 04:32:06 +0800

    adds config api service

---
+++ src/app/shared/services/api/config-apo/config-api.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpService } from '../../http/http.service';
+import { environment } from '@env/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ConfigApiService {
+  apiUrl = environment.API_URL;
+  apiPrefix = 'config';
+  constructor(private httpService: HttpService) {}
+
+  createConfig(config: any) {
+    return this.httpService.post(`${this.apiPrefix}`, config);
+  }
+
+  getConfigByName(name: string) {
+    return this.httpService.get(`${this.apiPrefix}/${name}`);
+  }
+
+  updateConfigByName(name: string, updateBody: any) {
+    return this.httpService.put(`${this.apiPrefix}/${name}`, updateBody);
+  }
+}
