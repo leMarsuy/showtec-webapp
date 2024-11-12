1beda56629f913e041009703eecbc790d3f09432
Christian Alcaraz <christianmalcaraz@gmail.com>
2024-11-13 02:57:22 +0800

    adds shared date filter input component module

---
+++ src/app/shared/components/date-filter-input/date-range-filter/date-range-filter.component.ts
@@ -0,0 +1,39 @@
+import { Component, Inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-date-range-filter',
+  templateUrl: './date-range-filter.component.html',
+  styleUrl: './date-range-filter.component.scss',
+  providers: [provideNativeDateAdapter()],
+})
+export class DateRangeFilterComponent {
+  dateRange: FormGroup = this.fb.group({
+    startDate: [null, Validators.required],
+    endDate: [null, Validators.required],
+  });
+
+  constructor(
+    private readonly fb: FormBuilder,
+    private readonly dialogRef: MatDialogRef<DateRangeFilterComponent>
+  ) {}
+
+  get isEndGreaterThenStart() {
+    const startDateControl = this.dateRange.get('startDate')?.value;
+    const endDateControl = this.dateRange.get('endDate')?.value;
+
+    if (!startDateControl) return false;
+    if (!endDateControl) return false;
+
+    const startDate = new Date(startDateControl).getTime();
+    const endDate = new Date(endDateControl).getTime();
+
+    return endDate >= startDate;
+  }
+
+  onSubmit() {
+    this.dialogRef.close(this.dateRange.getRawValue());
+  }
+}
