0b15e20ce3faa7b6a9939612faf7243ac32f0730
Christian Alcaraz <christianmalcaraz@gmail.com>
2024-12-03 15:47:17 +0800

    adds date service in util service

---
+++ src/app/shared/services/util/date/date.service.ts
@@ -0,0 +1,65 @@
+import { Injectable } from '@angular/core';
+import { QueryParams } from '@app/core/interfaces/query-params.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DateService {
+  private readonly ONE_DAY = 24 * 60 * 60 * 1000;
+  constructor() {}
+
+  getDateDiffInDays(firstDate: Date, secondDate: Date) {
+    return Math.round(this.getDiff(firstDate, secondDate) / this.ONE_DAY);
+  }
+
+  getDateDiffInMonths(firstDate: Date, secondDate: Date) {
+    return Math.round(this.getDiff(firstDate, secondDate) / this.ONE_DAY);
+  }
+
+  getDateDiffInYears(firstDate: Date, secondDate: Date) {
+    return Math.round(this.getDiff(firstDate, secondDate) / this.ONE_DAY);
+  }
+
+  getPastDate(date: Date, pastDays: number) {
+    return new Date(date.setDate(date.getDate() - pastDays));
+  }
+
+  getDateFloor(date: Date) {
+    date.setHours(0, 0, 0, 0);
+    return date;
+  }
+
+  getDateCeil(date: Date) {
+    date.setHours(23, 59, 59, 99);
+    return date;
+  }
+
+  formatDate(date: Date, options?: Intl.DateTimeFormatOptions) {
+    const format = options ?? {
+      month: 'short',
+      day: 'numeric',
+      year: 'numeric',
+    };
+
+    return date.toLocaleDateString('en-US', format);
+  }
+
+  getDiff(firstDate: Date, secondDate: Date) {
+    return new Date(secondDate).getTime() - new Date(firstDate).getTime();
+  }
+
+  dateToQueryParam(query: QueryParams, date: any) {
+    let params: any = query;
+
+    if (date && typeof date === 'object') {
+      const objectToString = JSON.stringify(date);
+      params = { ...params, date: objectToString };
+    }
+
+    if (date && typeof date !== 'object') {
+      params = { ...params, date };
+    }
+
+    return params;
+  }
+}
