a3161a1abb9a248b9bb1cf0e4c6b5330eea8bcd1
Christian Alcaraz <christianmalcaraz@gmail.com>
2025-01-06 04:44:51 +0800

    admintype user sidenav builder

--- src/app/modules/portal/components/sidenav/sidenav.component.ts
+++ src/app/modules/portal/components/sidenav/sidenav.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject, Input, OnDestroy } from '@angular/core';
 import { EXCLUDED_PATHS } from '@app/core/constants/nav-paths';
-import { NAV_ROUTES } from '@app/core/lists/nav-routes.list';
-import { selectUserPermissions } from '@app/core/states/user';
+import { UserType } from '@app/core/enums/user-type.enum';
+import { NAV_ROUTES, NavRoute } from '@app/core/lists/nav-routes.list';
+import { selectUser, selectUserPermissions } from '@app/core/states/user';
 import { environment } from '@env/environment';
 import { Store } from '@ngrx/store';
-import { Subject, takeUntil } from 'rxjs';
+import { combineLatest, map, Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-sidenav',
@@ -17,6 +18,17 @@ export class SidenavComponent implements OnDestroy {
   navList = NAV_ROUTES;
   @Input() expanded!: Boolean;
 
+  private readonly adminPermissions = NAV_ROUTES.reduce(
+    (acc: Record<string, string | boolean>[], routeGroup) => {
+      const paths = routeGroup.items.flatMap((route: NavRoute) => {
+        if (EXCLUDED_PATHS.includes(route.path)) return [];
+        return [{ path: route.path, hasAccess: true }];
+      }, []);
+      return [...acc, ...paths];
+    },
+    [],
+  );
+
   permissionPathMap: Record<string, boolean> = {};
   navGroupState: Record<string, boolean> = {};
 
@@ -26,34 +38,54 @@ export class SidenavComponent implements OnDestroy {
   private _destroyed$ = new Subject<void>();
 
   constructor() {
-    this.store
-      .select(selectUserPermissions())
-      .pipe(takeUntil(this._destroyed$))
-      .subscribe((permissions) => {
-        if (permissions) {
-          this.permissionPathMap = permissions?.reduce(
-            (acc, permission) => {
-              acc[permission.path] = permission.hasAccess;
-              return acc;
-            },
-            {} as Record<string, boolean>,
-          );
+    combineLatest([
+      this.store.select(selectUser()),
+      this.store.select(selectUserPermissions()),
+    ])
+      .pipe(
+        map(([user, permissions]) => ({ details: user, permissions })),
+        takeUntil(this._destroyed$),
+      )
+      .subscribe((user) => {
+        if (user.details.userType === UserType.ADMIN) {
+          this._setPermissionPathMap(this.adminPermissions);
+          this._setNavGroupState();
+          return;
+        }
 
-          for (const excludedPath of EXCLUDED_PATHS) {
-            this.permissionPathMap[excludedPath] = true;
-          }
-
-          this.navGroupState = this.navList.reduce(
-            (acc, navGroup) => {
-              acc[navGroup.group] = navGroup.items.some(
-                (navItem) => this.permissionPathMap[navItem.path],
-              );
-              return acc;
-            },
-            {} as Record<string, boolean>,
-          );
+        if (user.permissions) {
+          this._setPermissionPathMap(user.permissions);
+          this._setNavGroupState();
         }
       });
+  }
+
+  private _setPermissionPathMap(permissions: any) {
+    const remapped = permissions;
+
+    this.permissionPathMap = remapped.reduce(
+      (acc: any, permission: any) => {
+        acc[permission.path] = permission.hasAccess;
+        return acc;
+      },
+      {} as Record<string, boolean>,
+    );
+
+    for (const excludedPath of EXCLUDED_PATHS) {
+      this.permissionPathMap[excludedPath] = true;
+    }
+  }
+
+  private _setNavGroupState() {
+    this.navGroupState = this.navList.reduce(
+      (acc, navGroup) => {
+        acc[navGroup.group] = navGroup.items.some(
+          (navItem) => this.permissionPathMap[navItem.path],
+        );
+        return acc;
+      },
+      {} as Record<string, boolean>,
+    );
   }
 
   ngOnDestroy(): void {
