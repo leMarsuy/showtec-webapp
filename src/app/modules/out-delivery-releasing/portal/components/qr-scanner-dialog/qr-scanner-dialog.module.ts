1a61116567d261e39f748eadac0391bfdd712e87
Christian Alcaraz <christianmalcaraz@gmail.com>
2025-02-14 02:29:18 +0800

    added config

--- src/app/modules/out-delivery-releasing/portal/components/qr-scanner-dialog/qr-scanner-dialog.module.ts
+++ src/app/modules/out-delivery-releasing/portal/components/qr-scanner-dialog/qr-scanner-dialog.module.ts
@@ -6,9 +6,12 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSelectModule } from '@angular/material/select';
-import { NgxScannerQrcodeModule } from 'ngx-scanner-qrcode';
+import { LOAD_WASM, NgxScannerQrcodeModule } from 'ngx-scanner-qrcode';
 import { QrScannerDialogComponent } from './qr-scanner-dialog.component';
 
+LOAD_WASM('assets/wasm/ngx-scanner-qrcode.wasm').subscribe((res: any) => {
+  console.log('LOAD_WASM', res);
+});
 @NgModule({
   declarations: [QrScannerDialogComponent],
   exports: [QrScannerDialogComponent],
